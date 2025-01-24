225dcfeb955f8e3835d058386873ac03f5556a92
Tung Do <dothanhtung132@gmail.com>
2025-01-24 12:09:12 +1030

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Stop ads popup
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  Stop window.open if matched URLs
+// @author       Tung Do
+// @match        *://*/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const originalWindowOpen = window.open;
+    window.open = (...args) => {
+        if (/^(.*\.)?(shopee|lazada)\./.test(args[0])) return;
+        originalWindowOpen.apply(window, args);
+    };
+})();
